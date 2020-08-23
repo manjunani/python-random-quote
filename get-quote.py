ee6159c410579ec822a8d7001c61419e8be83853
Manjunatha Sai Uppu <manjunatha16.512@gmail.com>
2020-08-23 09:15:52 +0530

    small changes

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,12 @@
 import random
 def primary():
-  #print("Keep it logically awesome.")
-
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
   last = len(quotes) - 1
   rnd = random.randint(0,last)
 
-  print(quotes[rnd])
+  print(quotes[rnd],quotes[rnd-1])
 
 if __name__== "__main__":
   primary()
